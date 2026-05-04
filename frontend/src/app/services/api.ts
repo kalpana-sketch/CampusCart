fca7830cf402c3c01e1bcc28f1b5b91df67e52b5
kalpana-sketch <nemopal50@gmail.com>
2026-05-04 20:23:31 +0530

    added .env file in frontend

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add a request interceptor to add the JWT token to headers
@@ -13,6 +13,7 @@ api.interceptors.request.use(
     }
     return config;
   },
+
   (error) => {
     return Promise.reject(error);
   }
